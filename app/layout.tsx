1eb8d90082fc552aa16ee2c24a49ca452ff620f6
Vrishin Patel <vrishin_patel@outlook.com>
2024-07-04 11:12:03 +0530

    Update font to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
-import "./global.css";
-import { Header } from "components/layout";
-import type { ReactNode } from "react";
+import './global.css';
+
+import { Header } from 'components/layout';
+import { Poppins } from 'next/font/google';
+
+import type { ReactNode } from 'react';
+
+const PoppinsFont = Poppins({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: '400',
+	variable: '--font-poppins',
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={`${PoppinsFont.variable}`}>
 			<body>
 				<Header />
 				{children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
 export default function Page() {
-	return <div className="container mx-auto">Hello World!</div>;
+	return (
+		<>
+			<div className='container mx-auto flex h-[calc(100vh-4rem)] flex-col items-center justify-center gap-8 bg-[url("/hero-bg.gif")] bg-center bg-cover bg-no-repeat'>
+				<h1 className="max-w-[17ch] text-center text-7xl">
+					Advancing AI for Real-World Problems
+				</h1>
+				<p className="max-w-[54ch] text-center">
+					India's first Deep Learning Lab for AI Security! Build a
+					safer future with us. Develop AI solutions in Medicine,
+					Governance & more!
+				</p>
+
+				<button type="button">Explore Models</button>
+			</div>
+		</>
+	);
 }
