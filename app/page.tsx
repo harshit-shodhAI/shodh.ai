cef6644b9f01ec21aa1cbf2805e3adb7990b0934
2021ucp1382 <2021ucp1382@mnit.ac.in>
2024-07-20 11:40:13 +0000

    waitlist

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
+import { useState, FormEvent } from 'react';
 import data from "@/data/home";
 // import TwitterTimeline from "@/app/twitter";
-import TweetEmbed from 'react-tweet-embed'
+import TweetEmbed from 'react-tweet-embed';
 
 export default function Home() {
+  const [email, setEmail] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const mailtoLink = `mailto:sayhi@shodh.ai?subject=New Waitlist Signup&body=New waitlist signup with email: ${email}`;
+    window.location.href = mailtoLink;
+    setEmail('');
+  };
+
   return (
     <>
       <div className="text-white home">
@@ -13,7 +23,21 @@ export default function Home() {
           </div>{" "}
           <div className="text-3xl lg:text-6xl">{data.mainHeading[1]}</div>
           <div className="m-4 lg:text-xl lg:w-1/2">{data.mainSubtext}</div>
-          <TweetEmbed tweetId="1811448269140017540" options={{theme: 'dark' }} className="w-4/5 lg:w-1/2 twitter-embed"/>
+          <form 
+            onSubmit={handleSubmit}
+            className="flex flex-col lg:flex-row gap-4 lg:gap-8 justify-center items-center"
+          >
+            <div>Join the waitlist</div>
+            <input 
+              type="email" 
+              placeholder="Enter your email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="rounded w-3/5 text-black py-1 px-3 text-center text-feild border border-white focus:outline-none focus:border-blue-500"
+              required
+            />
+          </form>
+          <TweetEmbed tweetId="1811448269140017540" options={{ theme: 'dark' }} className="w-4/5 lg:w-1/2 twitter-embed" />
         </div>
       </div>
     </>
