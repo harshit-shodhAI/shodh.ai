6b61dcce1531d71fe1a4d3cca99fce588f2dc38d
2021ucp1382 <2021ucp1382@mnit.ac.in>
2024-07-20 16:13:12 +0000

    Arrow, Text change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, FormEvent } from 'react';
 import data from "@/data/home";
-// import TwitterTimeline from "@/app/twitter";
 import TweetEmbed from 'react-tweet-embed';
 
 export default function Home() {
@@ -20,22 +19,33 @@ export default function Home() {
         <div className="h-full text-center flex flex-col items-center justify-start mt-16 w-screen gap-3 lg:gap-4">
           <div className="text-4xl font-medium head-color lg:text-8xl">
             {data.mainHeading[0]}
-          </div>{" "}
+          </div>
           <div className="text-3xl lg:text-6xl">{data.mainHeading[1]}</div>
-          <div className="m-4 lg:text-xl lg:w-1/2">{data.mainSubtext}</div>
+          <div className="m-4 lg:text-xl lg:w-2/3">{data.mainSubtext}</div>
           <form 
             onSubmit={handleSubmit}
             className="flex flex-col lg:flex-row gap-4 lg:gap-8 justify-center items-center"
           >
-            <div>Join the waitlist</div>
-            <input 
-              type="email" 
-              placeholder="Enter your email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="rounded w-3/5 text-black py-1 px-3 text-center text-feild border border-white focus:outline-none focus:border-blue-500"
-              required
-            />
+            <div className='flex flex-col justify-start items-center lg:w-4/6 text-gray-200'>
+              <div>Shodh.ai is by invitation only</div>
+              <div>Enter your email for early access</div>
+            </div>
+            <div className="flex items-center w-3/5 lg:w-auto">
+              <input
+                type="email" 
+                placeholder="Enter your email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="rounded w-full text-black py-1 px-3 text-center text-field border border-white focus:outline-none focus:border-blue-500"
+                required
+              />
+              <button 
+                type="submit" 
+                className="ml-2 px-2 head-color-bg text-white rounded focus:outline-none"
+              >
+                ➔
+              </button>
+            </div>
           </form>
           <TweetEmbed tweetId="1811448269140017540" options={{ theme: 'dark' }} className="w-4/5 lg:w-1/2 twitter-embed" />
         </div>
